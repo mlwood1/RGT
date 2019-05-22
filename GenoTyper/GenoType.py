05c08cf312f80a633d5e2be9fa568d4bc2574c53
Hossam <hossam26644@live.com>
2019-05-22 11:39:29 +0100

    Repeat class created and used

--- GenoTyper/GenoType.py
+++ GenoTyper/GenoType.py
@@ -1,5 +1,5 @@
 '''docstring'''
-#from .Repeat import Repeat
+from .Repeat import Repeat
 
 class Genotype():
     """docstring for Genotype"""
@@ -18,34 +18,33 @@ class Genotype():
     def get_line_repeates(self, sequence, geno_table):
         window_length = len(self.repeat_unit)
         window_inside_repeates = False
-        number_of_current_repeats = 0
-        last_repeat_index = -1
 
         i = window_length
         while i < len(sequence): #sliding window
             window = sequence[i-window_length:i]
             if self.is_window_equals_repeat_unit(window, self.repeat_unit) and (not window_inside_repeates):
                 #if window detects a repeat unit, while it is not inside a repeat sequence
+                repeat = Repeat(i) #creat a repeat object
                 window_inside_repeates = True
-                number_of_current_repeats = 1
-                last_repeat_index = i
                 i = i+2 #Jumb one window
+
             elif self.is_window_equals_repeat_unit(window, self.repeat_unit) and window_inside_repeates:
                 #if it detects a repeat while inside the repeat sequence
-                number_of_current_repeats += 1
-                last_repeat_index = i
+                repeat.add_unit() #add a repeat unit count
+                repeat.change_last_unit_index(i)
                 i = i+2 #jumb one window
+
             elif (not(self.is_window_equals_repeat_unit(window, self.repeat_unit)) and window_inside_repeates) or (window_inside_repeates and i == len(sequence)):
                 '''  first condition: when the repeat ends, second is when the read is finished'''
                 '''  non repeat elements found in the repeat sequence '''
-                if i-last_repeat_index <= self.max_interrupt_tract:
+                if i-repeat.last_unit_index <= self.max_interrupt_tract:
                     #ignore if length is smaller than max interrupt tract
                     i += 1
                     continue
 
                 window_inside_repeates = False #if length is larger than max interrupt tract
-                if number_of_current_repeats >= self.min_size_repeate: #check that number of repeates is larger than the minimum size repeate
-                    self.add_repeat_to_genotable(number_of_current_repeats, geno_table)
+                if repeat.number_of_units >= self.min_size_repeate: #check that number of repeates is larger than the minimum size repeate
+                    self.add_repeat_to_genotable(repeat, geno_table)
             i +=1   
         return geno_table
    
@@ -59,8 +58,9 @@ class Genotype():
                 return False
         return True
 
-    def add_repeat_to_genotable(self, number_of_repeat_units, geno_table):
-
+    def add_repeat_to_genotable(self, repeat, geno_table):
+        number_of_repeat_units = repeat.number_of_units
+       
         if(number_of_repeat_units in geno_table):
             geno_table[number_of_repeat_units] += 1
         else:
--- GenoTyper/Repeat.py
+++ GenoTyper/Repeat.py
@@ -2,5 +2,12 @@
 
 class Repeat():
     """docstring for Genotype"""
-    def __init__(self):
-        pass
+    def __init__(self, last_unit_index, number_of_units=1, repeat_unit="CTG"):
+        self.last_unit_index = last_unit_index
+        self.number_of_units = number_of_units
+
+    def add_unit(self):
+        self.number_of_units +=1
+
+    def change_last_unit_index(self, index):
+        self.last_unit_index = index
