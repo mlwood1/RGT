ece234bb2a39279eb8da2d86d5e802359b9aee2a
Hossam <hossam26644@live.com>
2019-05-24 11:04:00 +0100

    repeat class sequence added

--- GenoTyper/GenoType.py
+++ GenoTyper/GenoType.py
@@ -3,7 +3,7 @@ from .Repeat import Repeat
 
 class Genotype():
     """docstring for Genotype"""
-    def __init__(self, reads, repeat_unit="CTG", min_size_repeate=3, max_interrupt_tract=3):
+    def __init__(self, reads, repeat_unit="CTG", min_size_repeate=3, max_interrupt_tract=6):
         self.repeat_unit = repeat_unit
         self.reads = reads
         self.min_size_repeate = min_size_repeate
@@ -20,41 +20,42 @@ class Genotype():
         window_inside_repeates = False
 
         i = window_length
+        repeat = None
         while i < len(sequence): #sliding window
             window = sequence[i-window_length:i]
-            if self.is_window_equals_repeat_unit(window, self.repeat_unit) and (not window_inside_repeates):
+            if self.is_window_equals_repeat_unit(window, self.repeat_unit, repeat) and (not window_inside_repeates):
                 #if window detects a repeat unit, while it is not inside a repeat sequence
-                repeat = Repeat(i) #creat a repeat object
+                repeat = Repeat(i, window) #creat a repeat object
                 window_inside_repeates = True
                 i = i+2 #Jumb one window
 
-            elif self.is_window_equals_repeat_unit(window, self.repeat_unit) and window_inside_repeates:
+            elif self.is_window_equals_repeat_unit(window, self.repeat_unit, repeat) and window_inside_repeates:
                 #if it detects a repeat while inside the repeat sequence
-                repeat.add_unit() #add a repeat unit count
-                repeat.change_last_unit_index(i)
+                repeat.add_unit(window,i) #add a repeat unit count
                 i = i+2 #jumb one window
 
-            elif (not(self.is_window_equals_repeat_unit(window, self.repeat_unit)) and window_inside_repeates) or (window_inside_repeates and i == len(sequence)):
+            elif (not(self.is_window_equals_repeat_unit(window, self.repeat_unit, repeat)) and window_inside_repeates) or (window_inside_repeates and i == len(sequence)):
                 '''  first condition: when the repeat ends, second is when the read is finished'''
-                '''  non repeat elements found in the repeat sequence '''
+                '''  non repeat elements found while in the repeat sequence '''
                 if i-repeat.last_unit_index <= self.max_interrupt_tract:
                     #ignore if length is smaller than max interrupt tract
                     i += 1
                     continue
-
                 window_inside_repeates = False #if length is larger than max interrupt tract
                 if repeat.number_of_units >= self.min_size_repeate: #check that number of repeates is larger than the minimum size repeate
                     self.add_repeat_to_genotable(repeat, geno_table)
+                repeat = None
+
             i +=1   
         return geno_table
    
   
-    def is_window_equals_repeat_unit(self, window, repeat_unit):
+    def is_window_equals_repeat_unit(self, window, repeat_unit, repeat_object):
         mismatch = False
         for i in range(0,len(window)):
-            if (window[i] != repeat_unit[i]) and (not mismatch):
+            if (window[i] != repeat_unit[i]) and (not mismatch) and (repeat_object != None):
                 mismatch = True
-            elif (window[i] != repeat_unit[i]) and mismatch:
+            elif (window[i] != repeat_unit[i]) and (mismatch or repeat_object == None):
                 return False
         return True
 
--- GenoTyper/Repeat.py
+++ GenoTyper/Repeat.py
@@ -2,13 +2,35 @@
 
 class Repeat():
     """docstring for Genotype"""
-    def __init__(self, last_unit_index, number_of_units=1, repeat_unit="CTG"):
+    def __init__(self, last_unit_index,window, number_of_units=1, repeat_unit="CTG"):
         self.last_unit_index = last_unit_index
         self.number_of_units = number_of_units
         self.start_index = last_unit_index - (number_of_units*len(repeat_unit)) #for future use
+        self.single_point_mutation_indexes = []
+        self.unconfirmed_units = 0
+        self.repeat_unit = repeat_unit
+        self.repeat_sequence = window
+        self.unconfirmed_sequence = "" #sequence of non pure repeates waiting for a confirmed unit to  be added
 
-    def add_unit(self):
-        self.number_of_units +=1
+    def add_unit(self, window, index): #index is the last base index in the sequence
+        if window == self.repeat_unit: #perfect match
+            self.number_of_units +=1
+            self.number_of_units += self.unconfirmed_units #add the unconfirmed units (points with SNPs)
+            self.unconfirmed_units = 0 #zero the points with SNPs
+            self.change_last_unit_index(index)
+            self.repeat_sequence += self.unconfirmed_sequence
+            self.unconfirmed_sequence = ""
+            self.repeat_sequence += window
+
+        else:
+            self.unconfirmed_units += 1
+            self.single_point_mutation_indexes.append(self.get_SNP_index(window, index))
+            self.unconfirmed_sequence += window
+    
+    def get_SNP_index(self, window, index):
+        for i in range(0,len(window)):
+            if (window[i] != self.repeat_unit[i]) :
+                return index -len(window)+i
 
     def change_last_unit_index(self, index):
         self.last_unit_index = index
