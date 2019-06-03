8a5f39f8823ddedb36ee13264ca17e75d619b242
Hossam <hossam26644@live.com>
2019-06-03 11:01:45 +0100

    excel exporter support list table values; table[key] = values[val1, val2,..] instead of table[key] = val

--- ExcelExporter/ExcelExport.py
+++ ExcelExporter/ExcelExport.py
@@ -12,7 +12,14 @@ class ExcelWriter():
         ws = self.wb.add_sheet(sheet_name)
         for key in table.keys():
             ws.write(row,0,key)
-            ws.write(row,1, table[key])
+            values = table[key]
+            if isinstance(values, list): #check if the value is a single value or a list
+                for idx, val in enumerate(values):
+                    print(idx, val)
+                    ws.write(row,1+idx, val)
+            else:
+                    ws.write(row,1, table[key])
+
             row+=1
     
     def save_file(self, file_name="DefaultName.xls"):
