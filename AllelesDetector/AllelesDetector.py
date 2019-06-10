e50ba9b78e568e8b425a556afe204efb31044e28
Hossam <hossam26644@live.com>
2019-06-10 16:34:32 +0100

    homo workaround implemented

--- AllelesDetector/AllelesDetector.py
+++ AllelesDetector/AllelesDetector.py
@@ -8,6 +8,7 @@ class AllelesDetector():
         #sorted_geno_list: sorted by abundance, 2D list => list of lists(key index 0, value 1)
         self.sorted_geno_list = (sorted(geno_table.items(), key=lambda x: x[1][0], reverse=True))
 
+        self.counts_table = counts_table
         self.possible_alleles_list = self.get_possible_alleles_list_from_sorted_geno_list()  
         peak_identifier = PeakIdentifier(counts_table)
         self.peak_repeat_counts = peak_identifier.get_peaks()
@@ -16,13 +17,48 @@ class AllelesDetector():
         matching_sequences = self.get_matches_between_peaks_and_possible_alleles_list()
 
         if len(matching_sequences) == 2:
-            return(matching_sequences, "ok hetero")
+            return([matching_sequences[0],matching_sequences[1], "ok hetero", str(self.peak_repeat_counts)])
         elif len(matching_sequences) >2:
-            return(matching_sequences[:1], "check, >2")
+            return([matching_sequences[0], matching_sequences[1], "check, >2", str(self.peak_repeat_counts)])
+        
         elif len(matching_sequences) == 1:
-            return([matching_sequences[1], matching_sequences[0]], "ok homo" )
-        elif len(matching_sequences) ==0:
-            return([self.sorted_geno_list[0][1][0],self.sorted_geno_list[1][1][0]])
+            new_matching_sequences = self.explore_if_a_close_allele_exists()
+            if len(new_matching_sequences)==2 :
+                return([new_matching_sequences[0],new_matching_sequences[1], "ok hetero", str(self.peak_repeat_counts)])
+            
+            return([matching_sequences[0], matching_sequences[0], "ok homo", str(self.peak_repeat_counts)] )
+        
+        elif len(matching_sequences) == 0:
+            new_matching_sequences = self.explore_if_a_close_allele_exists()
+            if len(new_matching_sequences)==2 :
+                return([new_matching_sequences[0],new_matching_sequences[1], "ok hetero", str(self.peak_repeat_counts)])
+            elif len(new_matching_sequences)==1:
+                return([new_matching_sequences[0], new_matching_sequences[0], "ok homo", str(self.peak_repeat_counts) ] )
+           
+            return([self.sorted_geno_list[0][1][0],self.sorted_geno_list[1][1][0], "check no matching peaks", str(self.peak_repeat_counts) ])
+
+    def explore_if_a_close_allele_exists(self):
+        new_matching_sequences = []
+        new_peak_repeat_counts = list(self.peak_repeat_counts)
+
+        first_key = list(self.counts_table)[0]
+        counts_min_threshold = self.counts_table[first_key]* 0.5
+
+        #adding near by points to peak
+        for number_of_repeat_counts in self.counts_table.keys():
+            count = self.counts_table[number_of_repeat_counts]
+           
+            if number_of_repeat_counts in new_peak_repeat_counts:
+                continue
+            if count > counts_min_threshold:
+                new_peak_repeat_counts.append(number_of_repeat_counts)
+      
+        #checking if a match happens with the near by points, now identified as new peaks
+        for possibe_allele in self.possible_alleles_list:
+            if (possibe_allele[1][1]) in new_peak_repeat_counts:
+                new_matching_sequences.append(possibe_allele[0])
+        return new_matching_sequences
+        
 
     def get_matches_between_peaks_and_possible_alleles_list(self):
         matching_sequences = []
