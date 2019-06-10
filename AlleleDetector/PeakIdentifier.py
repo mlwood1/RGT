5bb23ecd45942b79786a6e5c5bc6986da4e57465
Hossam <hossam26644@live.com>
2019-06-10 11:40:35 +0100

    AllelesDetector package created, peak identifier is implemented

---
+++ AlleleDetector/PeakIdentifier.py
@@ -0,0 +1,33 @@
+'''docstring'''
+from scipy import signal
+
+class PeakIdentifier():
+    """docstring for main"""
+    def __init__(self, counts_table):
+        #geno_table = dict(sorted(geno_table.items(), key=lambda x: x[1][0], reverse=True))
+        counts_table = dict(sorted(counts_table.items(), key=lambda x: x[0], reverse=False))
+
+        self.counts_array = self.get_counts_array(counts_table)
+        #peaks = self.get_peaks()
+        #self.get_alleles_using_peaks_from_genotable()
+
+    def get_peaks(self):
+        average = sum(self.counts_array)/len(self.counts_array)
+        #print(average)
+        #print(signal.find_peaks(self.counts_array, threshold=average))
+        ans = signal.find_peaks(self.counts_array, prominence=average, distance=5)
+        #print(self.counts_array)
+        #print(ans[0])
+        return(ans[0])
+
+    def get_counts_array(self,counts_table):
+        ''' get array from counts_table dictionary, the array index is the dictionary
+            key (repeat counts), zero padding is done
+        '''
+        counts_array = []
+
+        for key in counts_table.keys():
+            while key > len(counts_array):
+                counts_array.append(0)
+            counts_array.append(counts_table[key])
+        return(counts_array)      
