fb5871605d9716cb1cc6d5a8053a9b2930cad400
Hossam <hossam26644@live.com>
2019-06-13 11:22:54 +0100

    Excel exporter supports xlsx, by changing the library, xlsx supports number of rows > 65k

--- ExcelExporter/ExcelExport.py
+++ ExcelExporter/ExcelExport.py
@@ -1,36 +1,41 @@
 ''' docstring'''
-import xlwt
+from openpyxl import Workbook
 
 class ExcelWriter():
     """docstring for ClassName"""
     def __init__(self):
-        self.wb = xlwt.Workbook()
-
+        #self.wb = xlwt.Workbook()
+        self.wb = Workbook()
+        std = self.wb.get_sheet_by_name('Sheet')
+        self.wb.remove_sheet(std)
 
     def add_table_to_sheet(self, table, sheet_name):
-        row = 0
-        ws = self.wb.add_sheet(sheet_name)
+        row = 1
+        ws = self.wb.create_sheet(sheet_name)
+        ws.title = sheet_name
+
         for key in table.keys():
-            ws.write(row,0,key)
+            ws.cell(row=row, column=1, value=key)
             values = table[key]
             if isinstance(values, list): #check if the value is a single value or a list
                 for idx, val in enumerate(values):
-                    ws.write(row,1+idx, val)
-            else:
-                    ws.write(row,1, table[key])
+                    ws.cell(row=row, column=2+idx, value=val)
+
+            else:   
+                    ws.cell(row=row, column=2, value=table[key])
 
             row+=1
     
-    def save_file(self, file_name="DefaultName.xls"):
+    def save_file(self, file_name="DefaultName.xlsx"):
         self.wb.save(file_name)
 
     @staticmethod	
     def write_to_excel(table, sheet_name="DefaultName", file_name="DefaultName.xls"):
-        row = 0
-        wb = xlwt.Workbook() #Creat a new workbook
-        ws = wb.add_sheet(sheet_name)
+        row = 1
+        wb = Workbook()
+        ws = wb.create_sheet(sheet_name)
         for key in table.keys():
-            ws.write(row,0,key)
-            ws.write(row,1, table[key])
+            ws.cell(row=row, column=1, value=key)
+            ws.cell(row=row, column=2, value=table[key])
             row+=1
         wb.save(file_name)
