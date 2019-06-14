227efef239eb56b9fad05d337852fda356560758
Hossam <hossam26644@live.com>
2019-06-14 16:38:45 +0100

    plot changed to bar plot

--- CountsPlotter/CountsPlotter.py
+++ CountsPlotter/CountsPlotter.py
@@ -7,6 +7,10 @@ class CountsPlotter():
     def plot_counts_table(counts_table, export_directory):
         sorted_table = sorted(counts_table.items(),reverse=True)
         x, y = zip(*sorted_table)
-        graph = plt.plot(x,y)
-        plt.savefig(export_directory)
+        
+        graph = plt.bar(x,y, align='center', alpha=0.5)
+        plt.xticks(list(range(1,max(x))), list(range(1,max(x))),   fontsize=4.5)
+
+        #plt.show()
+        plt.savefig(export_directory, dpi=600)
         plt.clf()
