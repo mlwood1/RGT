7e666a81f1c0f322ad058266940c85fd035d08cf
Hossam <hossam26644@live.com>
2019-06-14 14:54:03 +0100

    matching sequence class created, wich has sequence and count attributes

---
+++ AllelesDetector/MatchingSequence.py
@@ -0,0 +1,7 @@
+'''docstring'''
+
+class MatchingSequence():
+    """docstring for Genotype"""
+    def __init__(self, sequence_string, repeat_units_count):
+        self.repeat_units_count = repeat_units_count
+        self.sequence_string = sequence_string
