e04a725a78177eaae5d1e631f4f1e6812aa9dc27
Hossam <hossam26644@live.com>
2019-06-17 11:17:27 +0100

    input checker module created

---
+++ interface/check_files.py
@@ -0,0 +1,41 @@
+import getopt
+import sys, os, glob
+
+
+
+def check_input_directory(input_directory):
+    if not(os.path.isdir(input_directory)):
+        print('invalid input directory')
+        sys.exit()
+    
+    samples = glob.glob(input_directory + "/*.fastq")
+    if len(samples)==0:
+        print('no fastq files found in the input directory')
+        sys.exit()
+
+
+def check_or_create_output_directory(output_directory):
+    if not os.path.isdir(output_directory):
+        try:
+            os.mkdir(output_directory)
+            os.mkdir(output_directory +"/Plots")
+            print("output directory created")
+        except:
+            print("cannot acces or create output directory") 
+            sys.exit()
+
+    if not os.path.isdir(output_directory + "/Plots"):
+            os.mkdir(output_directory +"/Plots")
+            print("Plots directory created inside output directory")
+
+    if not os.path.isdir(output_directory + "/FilesSpecificResults"):
+            os.mkdir(output_directory +"/FilesSpecificResults")
+            print("FilesSpecificResults created inside output directory")
+
+def check_settings_file(settings_file):
+    if not os.path.exists(settings_file):
+        print("settings file not be found") 
+        sys.exit()
+    if not settings_file.endswith('.json'):
+        print("settings file should be in json format") 
+        sys.exit()
