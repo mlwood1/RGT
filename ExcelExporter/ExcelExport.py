b051593615d5c2a37bda9ccde3ff9a0a9e7874eb
Hossam <hossam26644@live.com>
2019-06-21 16:10:25 +0100

    header style is done, columns width are adjusted to the column headers lenghts+10

--- ExcelExporter/ExcelExport.py
+++ ExcelExporter/ExcelExport.py
@@ -1,5 +1,7 @@
 ''' docstring'''
 from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+from string import ascii_lowercase
 
 class ExcelWriter():
     """docstring for ClassName"""
@@ -27,15 +29,27 @@ class ExcelWriter():
                     ws.cell(row=row, column=2, value=table[key])
 
             row+=1
-    
+        self.cols_adjust_size(ws, header_list)
+
+    def cols_adjust_size(self, ws, header_list):
+        for idx, header in enumerate(header_list):
+            size = len(header)+5
+            col = chr(ord("a") + idx)
+            ws.column_dimensions[col].width = size
+
+   
     def save_file(self, file_name="DefaultName.xlsx"):
         self.wb.save(file_name)
 
-    def write_table_header(self, worksheet, header_list):
-        for idx, col_name in enumerate(header_list): #put the table header
-            print(idx, col_name)
-            worksheet.cell(row=1, column=1+idx, value=col_name)
+    def write_table_header(self, ws, header_list):
 
+        fill_style = PatternFill(fill_type='solid',
+                start_color='FFFF00')
+      
+        for idx, col_name in enumerate(header_list): #put the table header
+            cell = ws.cell(row=1, column=1+idx, value=col_name)
+            cell.fill = fill_style
+        
 
     @staticmethod	
     def write_to_excel(table, sheet_name="DefaultName", file_name="DefaultName.xls"):
